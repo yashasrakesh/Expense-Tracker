e70095769e18c355d53681dc0417b15add96681a
Yashas R <yashasr@Yashass-MacBook-Air.local>
2026-05-18 20:38:43 +0530

    main.jsx changed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -71,6 +71,50 @@
 // 	</React.StrictMode>
 // );
 
+// import React from "react";
+
+// import ReactDOM from "react-dom/client";
+
+// import App from "./App.jsx";
+
+// import "./index.css";
+
+// import { BrowserRouter } from "react-router-dom";
+
+// import GridBackground from "./components/ui/GridBackgroun.jsx";
+
+// import {
+// 	ApolloClient,
+// 	InMemoryCache,
+// 	ApolloProvider,
+// } from "@apollo/client";
+
+// const client = new ApolloClient({
+// 	uri:
+// 		import.meta.env.MODE ===
+// 		"development"
+// 			? "https://expense-tracker-api-production-2e87.up.railway.app/graphql"
+// 			: "/graphql",
+
+// 	cache: new InMemoryCache(),
+
+// 	credentials: "include",
+// });
+
+// ReactDOM.createRoot(
+// 	document.getElementById("root")
+// ).render(
+// 	<React.StrictMode>
+// 		<BrowserRouter>
+// 			<GridBackground>
+// 				<ApolloProvider client={client}>
+// 					<App />
+// 				</ApolloProvider>
+// 			</GridBackground>
+// 		</BrowserRouter>
+// 	</React.StrictMode>
+// );
+
 import React from "react";
 
 import ReactDOM from "react-dom/client";
@@ -91,10 +135,7 @@ import {
 
 const client = new ApolloClient({
 	uri:
-		import.meta.env.MODE ===
-		"development"
-			? "https://expense-tracker-api-production-2e87.up.railway.app/graphql"
-			: "/graphql",
+		"https://expense-tracker-api-production-2e87.up.railway.app/graphql",
 
 	cache: new InMemoryCache(),
 
